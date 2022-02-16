72424ec28253e64ac1082f91664d879d8147ffc7
Austin van der Merwe <austin.vdmerwe@signif.co.za>
2022-02-16 12:50:10 +0200

    Test
    test

---
+++ StartUp.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Signify.Components;
+
+public partial class StartUp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Redirect("ReportTemplate.aspx");
+    }
+}
