8b0d5aae501c3b7d00fd9cf07bf1dd1c91b40059
Austin van der Merwe <austin.vdmerwe@signif.co.za>
2022-02-17 13:39:51 +0200

    Some stuff

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -11,8 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string something;
-            something = null;
+
         }
     }
 }
