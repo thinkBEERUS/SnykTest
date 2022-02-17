4839a361b022cdb1e22491612943da17446ac774
Austin van der Merwe <austin.vdmerwe@signif.co.za>
2022-02-17 08:50:18 +0200

    Test4

---
+++ SAST-Tester/Contact.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SAST_Tester
+{
+    public partial class Contact : Page
+    {
+        private string name;
+        private void SetName(string name)
+        {
+            name = name;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetName("Helloooo Pieter");
+        }
+    }
+}
